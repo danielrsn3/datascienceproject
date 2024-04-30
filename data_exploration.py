ac7c8311c6cbdff8b6117f34c83edb4bfc630c29
Madstp98 <Madstp@msn.com>
2024-04-30 10:23:52 +0200

    FInal Changes MTP

--- data_exploration.py
+++ data_exploration.py
@@ -11,9 +11,14 @@ vehicles.dtypes # Display the data types of each column
 from data_preprocessing import apply_data_types # Function from preprocessing
 apply_data_types(vehicles)
 vehicles.dtypes # Display the data types of each column
-
 vehicles['year'] = vehicles['year'].astype('Int64') # Treat year as numeric to get summary statistics
 vehicles.describe(include='all') # Get summary statistics of all variables
+vehicles.dtypes # Display the data types of each column
+
+
+
+
+###### DATA EXPLORATION ######
 
 # Histogram of the 'price' column with prices up to $100,000
 plt.figure(figsize=(12, 6))
@@ -24,15 +29,7 @@ plt.ylabel('Frequency')
 plt.grid(True)
 plt.show()
 
-# Scatter plot of price vs. year for vehicles priced up to $100,000
-plt.figure(figsize=(12, 8))
-plt.scatter(vehicles['year'], vehicles['price'], alpha=0.5, edgecolors='w', color='green')
-plt.title('Scatter Plot of Vehicle Price vs. Year (Prices up to $100,000)')
-plt.xlabel('Year')
-plt.ylabel('Price')
-plt.grid(True)
-plt.show()
-
+# HEXBIN
 # Filter the data to ensure both 'year' and 'price' have valid entries and price is up to $100,000
 filtered_data = vehicles[(vehicles['price'] <= 100000) & (vehicles['year'].notna())]
 # Hexbin plot with filtered data
@@ -47,12 +44,15 @@ plt.show()
 
 # Creating a more detailed boxplot for the 'price' column with a zoomed-in view
 plt.figure(figsize=(10, 6))
-plt.boxplot(vehicles['price'].dropna(), vert=False, flierprops=dict(marker='o', color='red', alpha=0.5))
+plt.boxplot(vehicles['price'], vert=False, flierprops=dict(marker='o', color='red', alpha=0.5))
 plt.xlabel('Price')
 plt.title('Boxplot of Vehicle Prices with Outlier Detection')
 plt.grid(True)
 plt.xlim(0, 100000)  # Limiting the x-axis to enhance detail around the typical price range
 plt.show()
+# Observations above 57300 should be removed.
+
+
 
 # Grouping data by manufacturer and calculating the average price per manufacturer
 manufacturer_price_avg = vehicles.groupby('manufacturer')['price'].mean().sort_values(ascending=False)
@@ -65,9 +65,17 @@ plt.ylabel('Average Price')
 plt.xticks(rotation=90)  # Rotating the manufacturer names for better visibility
 plt.grid(axis='y')
 plt.show()
+    # Remove motercycles / Multiple manufactures with very high average price
 
 # Filtering the data for entries where the manufacturer is 'mercedes-benz'
 mercedes_benz_data = vehicles[vehicles['manufacturer'] == 'mercedes-benz']
 # Displaying the filtered data
 mercedes_benz_data.sort_values(ascending=False, by='price')
     # Outliers
+
+
+
+
+
+
+
