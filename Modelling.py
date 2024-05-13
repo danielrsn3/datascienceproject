fe472f34a045f661eef1d4101fd08e1f5e7d4b0c
Troels1998 <troels-thomsen@hotmail.com>
2024-05-13 18:40:16 +0200

    Last changes

--- Modelling.py
+++ Modelling.py
@@ -67,7 +67,7 @@ print(f"Test MSE: {test_mse}") # 19290048.540239062
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-# 3
+# 3 
 knn_regressor = KNeighborsRegressor(n_neighbors=3)
 knn_regressor.fit(X_train, y_train)
 
@@ -81,7 +81,7 @@ test_predictions_knn = knn_regressor.predict(X_test)
 test_rmse_knn = np.sqrt(mean_squared_error(y_test, test_predictions_knn))
 print(f"Test RMSE: {test_rmse_knn}") # 4487.2253509261645
 
-# 4
+# 4 
 knn_regressor = KNeighborsRegressor(n_neighbors=4)
 knn_regressor.fit(X_train, y_train)
 
@@ -104,9 +104,9 @@ train_predictions_knn = knn_regressor.predict(X_train)
 train_rmse_knn = np.sqrt(mean_squared_error(y_train, train_predictions_knn))
 train_mae_knn = mean_absolute_error(y_train, train_predictions_knn)
 train_mse_knn = mean_squared_error(y_train, train_predictions_knn)
-print(f"Train RMSE: {train_rmse_knn}") # 3544.0293459580375 
-print(f"Train MAE: {train_mae_knn}") 
-print(f"Train MSE: {train_mse_knn}") 
+print(f"Train RMSE: {train_rmse_knn}") # 3555.023642533517 
+print(f"Train MAE: {train_mae_knn}") # 2162.8121966728277
+print(f"Train MSE: {train_mse_knn}") # 12638193.098972274
 
 # TEST 
 test_predictions_knn = knn_regressor.predict(X_test)
@@ -124,7 +124,7 @@ knn_regressor.fit(X_train, y_train)
 # TRAIN
 train_predictions_knn = knn_regressor.predict(X_train)
 train_rmse_knn = np.sqrt(mean_squared_error(y_train, train_predictions_knn))
-print(f"Train RMSE: {train_rmse_knn}") # 
+print(f"Train RMSE: {train_rmse_knn}") # 3706.2758012616196
 
 # TEST
 test_predictions_knn = knn_regressor.predict(X_test)
@@ -156,23 +156,25 @@ test_rmse_randomforest = np.sqrt(mean_squared_error(y_test, test_predictions_ran
 test_mae_randomforest = mean_absolute_error(y_test, test_predictions_randomforest)
 test_mse_randomforest = mean_squared_error(y_test, test_predictions_randomforest)
 
-print(f"Test RMSE: {test_rmse_randomforest}") # 3400.640261293944 med 500 n_estimators
+print(f"Test RMSE: {test_rmse_randomforest}") # 3400.640261293944 
 print(f"Test MAE: {test_mae_randomforest}") # 1892.8797055297316
 print(f"Test MSE: {test_mse_randomforest}") # 11564354.186733345
 
-# 3428.0649056250068 med 50 n_estimators
-# 3403.083633046189 med 1000 n_estimators
+# RMSE = 3428.0649056250068 with 50 n_estimators
+# RMSE = 3403.083633046189 with 1000 n_estimators
 
+
+# Plot of RF decission tree
 from sklearn.tree import plot_tree
 import matplotlib.pyplot as plt
 
 # Selecting a single tree from the forest
-single_tree = random_forest.estimators_[1]
+single_tree = random_forest.estimators_[0]
 
 # Visualizing the tree
 plt.figure(figsize=(20,10))
-# Adding fontsize parameter to increase text size
-plot_tree(single_tree, feature_names=X_train.columns, filled=True, max_depth=3, fontsize=8)  # Adjust fontsize as needed
+
+plot_tree(single_tree, feature_names=X_train.columns, filled=True, max_depth=3, fontsize=8)
 plt.show()
 
 
@@ -183,33 +185,27 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # Initialize the Gradient Boosting regressor
 gbr = GradientBoostingRegressor(n_estimators=1000, learning_rate=0.5, max_depth=5, random_state=42)
-gbr.fit(X_train, y_train) # Train the model
-train_y_pred = gbr.predict(X_train) # Predict on the test set
+gbr.fit(X_train, y_train) 
+train_y_pred = gbr.predict(X_train) 
 # Calculate the evaluation metrics
-train_rmse = np.sqrt(mean_squared_error(y_train, train_y_pred)) # Calculate the root mean squared error (RMSE)
-train_mae = mean_absolute_error(y_train, train_y_pred) # Calculate the mean absolute error (MAE)
-train_mse = mean_squared_error(y_train, train_y_pred) # Calculate the mean squared error (MSE)
+train_rmse = np.sqrt(mean_squared_error(y_train, train_y_pred))  
+train_mae = mean_absolute_error(y_train, train_y_pred) 
+train_mse = mean_squared_error(y_train, train_y_pred) 
 
-print("Train RMSE:", train_rmse)
-# 5625.007709969297 (n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42) # IKKE KØRT PÅ NY
-# 3834.3064543798773 (n_estimators=500, learning_rate=0.3, max_depth=5, random_state=42) # IKKE KØRT PÅ NY
-# 3412.153134635619 (n_estimators=1000, learning_rate=0.5, max_depth=5, random_state=42)
-print("Train MAE:", train_mae) # 2144.671269246954 for n=1000
-print("Train MSE:", train_mse) # 11642789.014203683 for n=1000
+print("Train RMSE:", train_rmse) # 1816.694616811483
+print("Train MAE:", train_mae) # 1259.0532114312662
+print("Train MSE:", train_mse) # 3300379.330751821  
 
 
-test_y_pred = gbr.predict(X_test) # Predict on the test set
+test_y_pred = gbr.predict(X_test)
 # Calculate the evaluation metrics
-test_rmse = np.sqrt(mean_squared_error(y_test, test_y_pred)) # Calculate the root mean squared error (RMSE)
-test_mae = mean_absolute_error(y_test, test_y_pred) # Calculate the mean absolute error (MAE)
-test_mse = mean_squared_error(y_test, test_y_pred) # Calculate the mean squared error (MSE)
+test_rmse = np.sqrt(mean_squared_error(y_test, test_y_pred)) 
+test_mae = mean_absolute_error(y_test, test_y_pred) 
+test_mse = mean_squared_error(y_test, test_y_pred) 
 
-print("Test RMSE:", test_rmse)
-# 5625.007709969297 (n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42) # IKKE KØRT PÅ NY
-# 3834.3064543798773 (n_estimators=500, learning_rate=0.3, max_depth=5, random_state=42) # IKKE KØRT PÅ NY
-# 3412.153134635619 (n_estimators=1000, learning_rate=0.5, max_depth=5, random_state=42)
-print("Test MAE:", test_mae) # 2144.671269246954 for n=1000
-print("Test MSE:", test_mse) # 11642789.014203683 for n=1000
+print("Test RMSE:", test_rmse) # 3412.457201897026
+print("Test MAE:", test_mae) # 2144.671269246954 
+print("Test MSE:", test_mse) # 11642789.014203683 
 
 # Display feature importances
 feature_importances = pd.DataFrame(gbr.feature_importances_,
